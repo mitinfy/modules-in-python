d0ae5c413737c3da0155f9aaf3127768930a7df0
mitindia <127289786+mitinfy@users.noreply.github.com>
2023-04-05 11:44:04 +0530

    Add files via upload

---
+++ mod_add.py
@@ -0,0 +1,17 @@
+# Creating user defined module called "mod_add"
+#save file as "mod_add.py"
+def add_num(a,b):#function for addition
+    sum=a+b;
+    return sum; #return value
+
+def sub_num(a,b):#function for subtraction
+    sub=a-b;
+    return sub; #return value
+
+def mul_num(a,b):#function for multiplication
+    mul=a*b;
+    return mul; #return value
+
+def div_num(a,b):#function for division
+    div=a/b;
+    return div; #return value
---
+++ modu_call.py
@@ -0,0 +1,13 @@
+#calling user defined modules
+import modu
+
+#renaming the module as following
+import user_mod as um
+
+# from modu module
+modu.wish("Ganesh")
+
+# from user_mod module as it called as alias
+um.mit_mod()
+
+
